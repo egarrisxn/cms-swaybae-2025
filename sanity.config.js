eba2c4212121ac2d9bb0c0358ce457028d15dc95
egarrisxn <egarrisxn@gmail.com>
2024-11-20 13:50:49 -0500

    first commit

--- sanity.config.js
+++ sanity.config.js
@@ -2,13 +2,14 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import {projectId, dataset} from './lib/api.js'
 
 export default defineConfig({
   name: 'default',
   title: 'swaybae-sanity',
 
-  projectId: '0mlp51cg',
-  dataset: 'production',
+  projectId: projectId || '',
+  dataset: dataset || '',
 
   plugins: [structureTool(), visionTool()],
 
