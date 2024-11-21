4ea12cfecbc69a27a48c1a4cee5b6b8fe30d9f98
egarrisxn <egarrisxn@gmail.com>
2024-11-20 21:55:12 -0500

    working with a template

--- sanity.config.js
+++ sanity.config.js
@@ -1,8 +1,13 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
+import {presentationTool} from 'sanity/presentation'
 import {visionTool} from '@sanity/vision'
+import {assist} from '@sanity/assist'
+import {unsplashImageAsset} from 'sanity-plugin-asset-source-unsplash'
+import {structure} from './lib/structure'
 import {schemaTypes} from './schemaTypes'
-import {projectId, dataset} from './lib/api.js'
+import {projectId, dataset, apiVersion, previewUrl} from './lib/api'
+import * as resolve from './lib/resolve'
 
 export default defineConfig({
   name: 'default',
@@ -11,7 +16,23 @@ export default defineConfig({
   projectId: projectId || '0mlp51cg',
   dataset: dataset || 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [
+    structureTool({
+      structure,
+    }),
+    presentationTool({
+      previewUrl: {
+        origin: previewUrl || 'http://localhost:3000',
+        previewMode: {
+          enable: '/api/draft-mode/enable',
+        },
+      },
+      resolve,
+    }),
+    visionTool({defaultApiVersion: apiVersion}),
+    unsplashImageAsset(),
+    assist(),
+  ],
 
   schema: {
     types: schemaTypes,
--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,6 +1,24 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+import person from './documents/person'
+import page from './documents/page'
+import post from './documents/post'
+import callToAction from './objects/callToAction'
+import infoSection from './objects/infoSection'
+import settings from './singletons/settings'
+import link from './objects/link'
+import blockContent from './objects/blockContent'
 
-export const schemaTypes = [post, author, category, blockContent]
+// Export an array of all the schema types.  This is used in the Sanity Studio configuration. https://www.sanity.io/docs/schema-types
+
+export const schemaTypes = [
+  // Singletons
+  settings,
+  // Documents
+  page,
+  post,
+  person,
+  // Objects
+  blockContent,
+  infoSection,
+  callToAction,
+  link,
+]
